c0b8aed4fa9d728507174231d541447e6c378754
VishwajeetKR22 <118885646+VishwajeetKR22@users.noreply.github.com>
2025-04-23 02:12:01 +0530

    First of This

---
+++ Program Codes/UseStrings.java
@@ -0,0 +1,6 @@
+public class UseStrings {
+    public static void main(String[] args) {
+        String message = "Java is the One?";
+        System.out.println("The message is: " + message);
+    }
+}
